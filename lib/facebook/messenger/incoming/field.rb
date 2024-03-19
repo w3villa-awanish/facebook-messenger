eac2d78e4edc37075d875b257865f65e8f34286d
w3villa-awanish <awanish.singh@w3villa.com>
2024-03-19 17:49:59 +0530

    rename file leadgen to field

--- lib/facebook/messenger/incoming/field.rb
+++ lib/facebook/messenger/incoming/field.rb
@@ -3,9 +3,8 @@ module Facebook
       module Incoming
         # The Message echo class represents an incoming Facebook Messenger message
         # @see https://developers.facebook.com/docs/messenger-platform/reference/webhook-events/message-echoes
-        class Leadgen < Message
+        class Field < Message
         end
       end
     end
   end
-  
